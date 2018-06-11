abfe70261c8aba008ba1d273ab80d97902d1d972
manxiaqu <1847234666@qq.com>
2018-06-11 11:09:16 +0800

    Change HD Derivation Path

--- accounts/hd.go
+++ accounts/hd.go
@@ -27,17 +27,20 @@ import (
 // DefaultRootDerivationPath is the root path to which custom derivation endpoints
 // are appended. As such, the first account will be at m/44'/60'/0'/0, the second
 // at m/44'/60'/0'/1, etc.
-var DefaultRootDerivationPath = DerivationPath{0x80000000 + 44, 0x80000000 + 60, 0x80000000 + 0, 0}
+// ASCII for 'G' 'E' 'T'; Decimal 4670804; Hexadecimal 0x474554.
+var DefaultRootDerivationPath = DerivationPath{0x80000000 + 44, 0x80000000 + 4670804, 0x80000000 + 0, 0}
 
 // DefaultBaseDerivationPath is the base path from which custom derivation endpoints
 // are incremented. As such, the first account will be at m/44'/60'/0'/0, the second
 // at m/44'/60'/0'/1, etc.
-var DefaultBaseDerivationPath = DerivationPath{0x80000000 + 44, 0x80000000 + 60, 0x80000000 + 0, 0, 0}
+// ASCII for 'G' 'E' 'T'; Decimal 4670804; Hexadecimal 0x474554.
+var DefaultBaseDerivationPath = DerivationPath{0x80000000 + 44, 0x80000000 + 4670804, 0x80000000 + 0, 0, 0}
 
 // DefaultLedgerBaseDerivationPath is the base path from which custom derivation endpoints
 // are incremented. As such, the first account will be at m/44'/60'/0'/0, the second
 // at m/44'/60'/0'/1, etc.
-var DefaultLedgerBaseDerivationPath = DerivationPath{0x80000000 + 44, 0x80000000 + 60, 0x80000000 + 0, 0}
+// ASCII for 'G' 'E' 'T'; Decimal 4670804; Hexadecimal 0x474554.
+var DefaultLedgerBaseDerivationPath = DerivationPath{0x80000000 + 44, 0x80000000 + 4670804, 0x80000000 + 0, 0}
 
 // DerivationPath represents the computer friendly version of a hierarchical
 // deterministic wallet account derivaion path.
