77b954a6216eeaae4d5d9dd66c1fc25b20f00171
hugo <hugopku.com>
2018-06-14 19:41:15 +0800

    update main cli flag

--- cmd/gtrustee/trustee_cli.go
+++ cmd/gtrustee/trustee_cli.go
@@ -1,31 +1,57 @@
 package main
 
 import (
-	"flag"
 	"github.com/themis-network/go-themis/trustee"
 	"log"
+	"gopkg.in/urfave/cli.v1"
+	"os"
+	"fmt"
 )
 
 var(
-	datadir = flag.String("datadir", "", `--datadir path_to_data`)
-	endpoint = flag.String("endpoint", "", `--endpoint ip:port`)
+	app = cli.NewApp()
+
+	dataDirFlag = cli.StringFlag{
+		Name:  "datadir",
+		Usage: "Data directory for keystore",
+	}
+	endpointFlag = cli.StringFlag{
+		Name:  "endpoint",
+		Usage: "ip:port, eg. 192.168.1.102:8090",
+	}
 )
 
-func main(){
+func init(){
+	app.Name = "Trustee service"
+	app.Usage = "gtrustee [options]"
+	app.Copyright = "Copyright 2017-2018 The go-themis Authors"
+	app.Version = "0.5.1"
+	app.Action = gtrustee
+	flags := []cli.Flag{
+		dataDirFlag,
+		endpointFlag,
+	}
+	app.Flags = append(app.Flags, flags...)
+}
 
-	flag.Parse()
+func gtrustee(ctx *cli.Context){
 
-	if *endpoint == "" {
+	if !ctx.GlobalIsSet(endpointFlag.Name){
 		log.Fatal("Error, need --endpoint ip:port")
 	}
 
 	config := trustee.Config{
-		DataDir: *datadir,
-		Endpoint: *endpoint,
+		DataDir: ctx.GlobalString(dataDirFlag.Name),
+		Endpoint: ctx.GlobalString(endpointFlag.Name),
 	}
 
 	var trusteeServer = trustee.New(config)
 	trusteeServer.Start()
+}
 
-	//trustee.GetContractData()
+func main(){
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
