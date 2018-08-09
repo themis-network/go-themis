9c211aa598e880dabb450d790fc1fc21de998465
HarleyHu <15033157073@163.com>
2018-08-09 18:26:45 +0800

    new dpos test for the  determinacy of sorted result

--- consensus/dpos/dpos_test.go
+++ consensus/dpos/dpos_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 	"time"
+	"sort"
 )
 
 func TestRandom(t *testing.T) {
@@ -17,4 +18,37 @@ func TestRandom(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		fmt.Println(rand.GenRandom())
 	}
+}
+
+const (
+	testLength int = 26
+)
+
+func TestSortDeterminacy(t *testing.T) {
+	var intArr = [testLength]int{100,101,102,103,104,105,106,107,108,109, 110,111,112,113,114,115,116,117,118,119, 120,121,122,123,124,125}
+	var uint64Arr = [testLength]uint64{10,2,10,2,10,2,10,2,10,2, 1,2,1,2,1,2,1,2,1,2, 0,10,0,10,0,10}
+	
+	var originNums sortNumSlice
+	for i := 0; i < testLength ; i++ {
+		var tmp = &sortNum {
+			serial: intArr[i],
+			num: uint64Arr[i],
+		}
+		originNums = append(originNums, tmp)
+	}
+	var sortNumsA sortNumSlice
+	var sortNumsB sortNumSlice
+	sortNumsA = originNums
+	sort.Sort(sortNumsA)
+	for i := 0; i<testLength; i++ {
+		sortNumsB = originNums
+		sort.Sort(sortNumsB)
+		//fmt.Println(len(sortNumsA), len(sortNumsB))
+		for j := 0; j<testLength; j++ {
+			if sortNumsA[j].serial != sortNumsB[j].serial {
+				t.Errorf("test TestSortDeterminacy() failed.")
+			}	
+		}
+	}
+	fmt.Println("TestSortDeterminacy() successed.")
 }
